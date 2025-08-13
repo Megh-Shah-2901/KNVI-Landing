23ffd9d0b823d514de90184a8e8e13ed04bb0278
Megh Shah <86917130+Megh-Shah-2901@users.noreply.github.com>
2025-08-13 21:05:16 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/KNVI-Landing',
+  base: '/',
   plugins: [
     react(),
     mode === 'development' &&
